5838ce72aa466e1ae7e75ad625ed83674c3ed656
Emre YÜKSEL <equesse78@gmail.com>
2023-10-31 19:20:50 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerManager = CustomManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+
+        class CustomManager
+        {
+            private static CustomManager _customerManager;
+
+                private CustomManager()
+            {
+
+
+            }
+            public static CustomManager CreateAsSingleton()
+            {
+                if( _customerManager == null)
+                {
+                    _customerManager = new CustomManager();
+                }
+                return _customerManager;
+            }
+            public  void Save()
+            {
+                Console.WriteLine("Save");
+
+            }
+        }
+    }
+}
