e5e60c30559024cb1febf44a879106bf94109705
Emre YÜKSEL <equesse78@gmail.com>
2023-12-06 19:18:23 +0300

    a

---
+++ Facade/Program.cs
@@ -0,0 +1,78 @@
+﻿using Facade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+    class Logging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged");
+
+
+        }
+    }
+    internal interface ILogging
+    {
+        void Log();
+    }
+        class Caching:ICaching
+        {
+            public void Cache()
+            {
+                Console.WriteLine("Cached");
+            }
+
+        }
+
+    internal interface ICaching
+    {
+        void Cache();
+    }
+
+    class Authorize:IAuthorize
+    {
+            public void CheckUser()
+            {
+                Console.WriteLine("User Checked");
+            }
+
+        }
+
+    internal interface IAuthorize
+    {   
+        void CheckUser();
+    }
+}
+    class CustomerManager
+    {
+    private ILogging _logging;
+    ICaching _caching;
+    IAuthorize _authorize;
+    public CustomerManager(ILogging logging, ICaching caching, IAuthorize authorize)
+    {
+        _logging = logging;
+        _caching = caching;
+        _authorize = authorize;
+    }
+    public void Save()
+    {
+        _logging.Log();
+        _caching.Cache();
+        _authorize.CheckUser();
+        Console.WriteLine("Saved");
+    }
+}
+
--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -10,8 +10,42 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
+            Customer customer = new Customer { FirstName="Emre",LastName="yuksel",City="Bursa",Id=2};
+            Console.WriteLine(customer.FirstName);
 
+            Customer customer2 = (Customer)customer.Clone();
+            customer2.FirstName = "Ege";
 
+            Console.WriteLine(customer.FirstName);
+            Console.WriteLine(customer2.FirstName);
+            Console.ReadLine();
         }
     }
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class Customer : Person
+    {
+        public string City { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+    public class Employee : Person
+    {
+        public decimal Salary { get; set; }
+
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+
+
 }
